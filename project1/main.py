6029af947f1cd4a34b19d49279efd4714eb88367
Mark Muetzelfeldt <markmuetz@gmail.com>
2016-02-05 10:12:01 +0000

    Improve line lengths.

--- project1/main.py
+++ project1/main.py
@@ -11,11 +11,12 @@ if __name__ == '__main__':
     # Allows easy running of individual tasks by e.g. python main.py A B
     # defaults to all.
     if len(sys.argv) == 1:
-	tasks = OrderedDict(A=True, B=True, C=True, D=True, E=True, F=True)
+        tasks = OrderedDict(A=True, B=True, C=True, D=True, E=True, F=True)
     else:
-	tasks = OrderedDict(A=False, B=False, C=False, D=False, E=False, F=False)
-	for arg in sys.argv[1:]:
-	    tasks[arg] = True
+        tasks = OrderedDict(A=False, B=False, C=False,
+                            D=False, E=False, F=False)
+        for arg in sys.argv[1:]:
+            tasks[arg] = True
     print(tasks)
 
     # Calculate some periods.
@@ -27,57 +28,59 @@ if __name__ == '__main__':
     print('one period: {}'.format(one_period))
 
     if tasks['A']:
-	settings = sim.init_settings()
-	plot_output(*sim.enso_oscillator(one_period, nt, **settings))
-	raw_input('Press enter to continue')
+        settings = sim.init_settings()
+        plot_output(*sim.enso_oscillator(one_period, nt, **settings))
+        raw_input('Press enter to continue')
 
     if tasks['B']:
-	settings = sim.init_settings(mu0=0.66)
-	plot_output(*sim.enso_oscillator(five_periods, nt, **settings), clear=False)
+        settings = sim.init_settings(mu0=0.66)
+        plot_output(*sim.enso_oscillator(five_periods, nt, **settings),
+                    clear=False)
 
-	settings = sim.init_settings(mu0=0.67)
-	plot_output(*sim.enso_oscillator(five_periods, nt, **settings), clear=False)
-	raw_input('Press enter to continue')
+        settings = sim.init_settings(mu0=0.67)
+        plot_output(*sim.enso_oscillator(five_periods, nt, **settings),
+                    clear=False)
+        raw_input('Press enter to continue')
 
     if tasks['C']:
-	settings = sim.init_settings(epsilon=0.1)
-	plot_output(*sim.enso_oscillator(five_periods, nt, **settings))
-	raw_input('Press enter to continue')
+        settings = sim.init_settings(epsilon=0.1)
+        plot_output(*sim.enso_oscillator(five_periods, nt, **settings))
+        raw_input('Press enter to continue')
 
-	settings = sim.init_settings(epsilon=0.1, mu0=0.67)
-	plot_output(*sim.enso_oscillator(five_periods, nt, **settings))
-	raw_input('Press enter to continue')
+        settings = sim.init_settings(epsilon=0.1, mu0=0.67)
+        plot_output(*sim.enso_oscillator(five_periods, nt, **settings))
+        raw_input('Press enter to continue')
 
     if tasks['D']:
-	tau=12/sim.TIME_SCALE # N.B. non-dimensionalised
-	settings = sim.init_settings(epsilon=0.1, mu0=0.75, mu_ann=0.2, tau=tau,
-		                     mode='annual_cycle')
-	plot_output(*sim.enso_oscillator(one_period * 4, nt, **settings))
-	raw_input('Press enter to continue')
+        tau=12/sim.TIME_SCALE # N.B. non-dimensionalised
+        settings = sim.init_settings(epsilon=0.1, mu0=0.75, mu_ann=0.2, tau=tau,
+                                     mode='annual_cycle')
+        plot_output(*sim.enso_oscillator(one_period * 4, nt, **settings))
+        raw_input('Press enter to continue')
 
 
     if tasks['E']:
-	tau=12/sim.TIME_SCALE # N.B. non-dimensionalised
-	tau_cor=1/30/sim.TIME_SCALE # N.B. non-dimensionalised
-	settings = sim.init_settings(epsilon=0.1, mu0=0.75, mu_ann=0.2, tau=tau,
-				     f_ann=0.02, f_ran=0.2, tau_cor=tau_cor,
-		                     mode='annual_cycle')
-	plot_output(*sim.enso_oscillator(one_period * 4, nt, **settings))
-	raw_input('Press enter to continue')
+        tau=12/sim.TIME_SCALE # N.B. non-dimensionalised
+        tau_cor=1/30/sim.TIME_SCALE # N.B. non-dimensionalised
+        settings = sim.init_settings(epsilon=0.1, mu0=0.75, mu_ann=0.2, tau=tau,
+                                     f_ann=0.02, f_ran=0.2, tau_cor=tau_cor,
+                                     mode='annual_cycle')
+        plot_output(*sim.enso_oscillator(one_period * 4, nt, **settings))
+        raw_input('Press enter to continue')
 
     if tasks['F']:
-	clear_plots()
-	nperiods = 10
-	# Set dt to one day by calculating nt correspondingly.
-	nt = int(one_period * nperiods * 30)
-	tau=12/sim.TIME_SCALE # N.B. non-dimensionalised
-	tau_cor=1/30/sim.TIME_SCALE # N.B. non-dimensionalised
-	settings = sim.init_settings(epsilon=0.1, mu0=0.75, mu_ann=0.2, tau=tau,
-				     f_ann=0.02, f_ran=0.2, tau_cor=tau_cor,
-		                     mode='annual_cycle', debug=False)
-	for h0 in np.linspace(-0.1, 0.1, 21):
-	    print(h0)
-	    settings['h0'] = h0
-	    plot_output(*sim.enso_oscillator(one_period * nperiods, nt, **settings), clear=False)
-	raw_input('Press enter to continue')
+        clear_plots()
+        nperiods = 10
+        # Set dt to one day by calculating nt correspondingly.
+        nt = int(one_period * nperiods * 30)
+        tau=12/sim.TIME_SCALE # N.B. non-dimensionalised
+        tau_cor=1/30/sim.TIME_SCALE # N.B. non-dimensionalised
+        settings = sim.init_settings(epsilon=0.1, mu0=0.75, mu_ann=0.2, tau=tau,
+                                     f_ann=0.02, f_ran=0.2, tau_cor=tau_cor,
+                                     mode='annual_cycle', debug=False)
+        for h0 in np.linspace(-0.1, 0.1, 21):
+            print(h0)
+            settings['h0'] = h0
+            plot_output(*sim.enso_oscillator(one_period * nperiods, nt, **settings), clear=False)
+        raw_input('Press enter to continue')
 
