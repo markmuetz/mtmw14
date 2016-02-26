2093ff887082ceb718eac6676232df9743f645af
Mark Muetzelfeldt <markmuetz@gmail.com>
2016-02-26 17:21:09 +0000

    Fix some issues to get code usable - runs for 1 day and produces clockwise gyre.
    Run for 30 days and looks v similar to analytical soln.
    Change to meshgrid ij option to get x-y in arrays correct.
    Fix typo in u_v (u at v) averaging to get averages calc'd correctly.

--- project2/gyresim.py
+++ project2/gyresim.py
@@ -20,25 +20,30 @@ def init_settings(**kwargs):
     return settings
 
 
-def gyre_sim(eta0, u0, v0, timelength, nt, x, y, f0, B, g, gamma, rho, H, tau0, L):
+def gyre_sim(t0, eta0, u0, v0, timelength, nt, x, y, f0, B, g, gamma, rho, H, tau0, L):
     eta = eta0
     u = u0
     v = v0
     #etas, us, vs = [eta0], [u0], [v0]
-    times = np.linspace(0, timelength, nt + 1)
+    times = np.linspace(t0, t0 + timelength, nt + 1)
     dt = times[1] - times[0]
-    dx = x[0, 1] - x[0, 0]
-    dy = y[1, 0] - y[0, 0]
+    dx = x[1, 0] - x[0, 0]
+    dy = y[0, 1] - y[0, 0]
     print('dx={}, dy={}, dt={}'.format(dx, dy, dt))
-    c_x, c_y = dt/dx, dt/dy
-    c = c_x**2 + c_y**2
-    phi = f0 * dt
-    print('c={}, , phi={}'.format(c, phi))
-    if c > 0.5:
-        print('Warning, runnint with c={} (>0.5)'.format(c))
-    if phi > 1:
-        print('Warning, runnint with phi={} (>1)'.format(phi))
-    tau_x = tau0 * - np.cos(np.pi * y / L)
+    c_x, c_y = np.sqrt(g * H) * dt/dx, np.sqrt(g * H) * dt/dy
+    # TODO: how does squaring change dimd c?
+    c2 = c_x**2 + c_y**2
+    print('c2={}'.format(c2))
+    if c2 > 0.25:
+        print('Warning, running with c2={} (>0.25)'.format(c2))
+    # TODO: How to calc dimd phi?
+    # phi = f0 * dt
+    #if phi > 1:
+        #print('Warning, runnint with phi={} (>1)'.format(phi))
+    tau_x = - tau0 * np.cos(np.pi * y / L)
+    #plt.figure('wind')
+    #plt.clf()
+    #plt.contour(x, y, tau_x)
     tau_y = np.zeros_like(y)
 
     v_u = np.zeros_like(u0)
@@ -50,7 +55,7 @@ def gyre_sim(eta0, u0, v0, timelength, nt, x, y, f0, B, g, gamma, rho, H, tau0, 
         for j in range(2):
             if (i + j) % 2 == 0:
                 v_u[1:-1, :] = (v[:-1, :-1] + v[1:, :-1] +
-                                v[-1:, 1:]  + v[1:, 1:]) / 4
+                                v[:-1, 1:]  + v[1:, 1:]) / 4
                 u[1:-1, :] = (+ u[1:-1, :] 
                               + (f0 + B * (y[:-1, :] + y[1:, :]) / 2) * dt * v_u[1:-1, :] 
                               - g * dt * (eta[1:, :] - eta[:-1, :]) / dx 
@@ -58,7 +63,7 @@ def gyre_sim(eta0, u0, v0, timelength, nt, x, y, f0, B, g, gamma, rho, H, tau0, 
                               + tau_x[:-1, :] * dt / (rho * H))
             else:
                 u_v[:, 1:-1] = (u[:-1, :-1] + u[1:, :-1] +
-                                u[-1:, 1:]  + u[1:, 1:]) / 4
+                                u[:-1, 1:]  + u[1:, 1:]) / 4
                 v[:, 1:-1] = (+ v[:, 1:-1] 
                               - (f0 + B * (y[:, :-1] + y[:, 1:]) / 2) * dt * u_v[:, 1:-1] 
                               - g * dt * (eta[:, 1:] - eta[:, :-1]) / dy 
@@ -76,16 +81,19 @@ def gyre_sim(eta0, u0, v0, timelength, nt, x, y, f0, B, g, gamma, rho, H, tau0, 
         #eta[:, -1] = 0
 
         if i % 100 == 0:
-            print(t)
+            print('{}: u min,max={},{}'.format(t, u.min(), u.max()))
+            u_grid = (u[:-1, :] + u[1:, :]) / 2
+            v_grid = (v[:, :-1] + v[:, 1:]) / 2
+            plt.figure(1)
             plt.clf()
-            plt.title('u')
-            try:
-                cs = plt.contour(x, y, u[:-1, :])
-            except:
-                pass
-            plt.clabel(cs, inline=1, fontsize=10)
-            plt.pause(0.1)
-            #r = raw_input()
+            cs = plt.contour(x, y, u_grid)
+            plt.figure(2)
+            plt.clf()
+            cs = plt.contour(x, y, v_grid)
+            plt.figure(3)
+            plt.clf()
+            plt.quiver(x[::5, ::5], y[::5, ::5], u_grid[::5, ::5], v_grid[::5, ::5])
+            plt.pause(0.01)
             r = 'c'
             if r == 'q':
                 break
@@ -94,8 +102,6 @@ def gyre_sim(eta0, u0, v0, timelength, nt, x, y, f0, B, g, gamma, rho, H, tau0, 
         #us.append(u)
         #vs.append(v)
     return eta, u, v
-
-
 
 
 def analytical_steady_state(x, y, L, f0, B, g, gamma, rho, H, tau0):
@@ -114,47 +120,52 @@ def analytical_steady_state(x, y, L, f0, B, g, gamma, rho, H, tau0):
                      (np.exp(b) - np.exp(a)))
     eta0 = 0
 
-    u_st = (-tau0 / (np.pi * gamma * rho * H) *
-            f1(x / L) * np.cos(np.pi * y / L))
-    v_st = (tau0 / (np.pi * gamma * rho * H) * 
-            f2(x / L) * np.sin(np.pi * y / L))
-    eta_st = (eta0 + tau0 / (np.pi * gamma * rho * H) * f0 * L / g *
-              (gamma / (f0 * np.pi) * f2(x / L) * np.sin(np.pi * y / L) +
-               1 / np.pi * f1(x / L) *
-               (np.sin(np.pi * y / L) * (1 + B * y / f0) + 
-                B * L / (f0 * np.pi) * np.cos(np.pi * y / L))))
+    u_st = (- tau0 / (np.pi * gamma * rho * H) *
+            + f1(x / L) * np.cos(np.pi * y / L))
+    v_st = (+ tau0 / (np.pi * gamma * rho * H) * 
+            + f2(x / L) * np.sin(np.pi * y / L))
+    eta_st = (+ eta0 + tau0 / (np.pi * gamma * rho * H) * f0 * L / g *
+              + (gamma / (f0 * np.pi) * f2(x / L) * np.sin(np.pi * y / L)
+                 + 1 / np.pi * f1(x / L) *
+                 + (np.sin(np.pi * y / L) * (1 + B * y / f0) 
+                 + B * L / (f0 * np.pi) * np.cos(np.pi * y / L))))
 
     return u_st, v_st, eta_st
 
 if __name__ == '__main__':
     plt.ion()
     settings = init_settings()
-    nx, ny = 1000, 1000
+    nx, ny = 51, 51
     L = 1e6
     x = np.linspace(0, L, nx)
     y = np.linspace(0, L, ny)
-    x, y = np.meshgrid(x, y)
+    x, y = np.meshgrid(x, y, indexing='ij')
     if False:
         u_st, v_st, eta_st = analytical_steady_state(x=x, y=y, **settings)
-        plt.figure(1)
-        plt.clf()
-        plt.quiver(x, y, u_st, v_st)
-        plt.figure(2)
+        plt.figure(21)
         plt.clf()
         plt.title('u')
         cs = plt.contour(x, y, u_st)
         plt.clabel(cs, inline=1, fontsize=10)
-        plt.figure(3)
+        plt.figure(22)
         plt.clf()
         plt.title('v')
         cs = plt.contour(x, y, v_st)
+        plt.figure(23)
         plt.clabel(cs, inline=1, fontsize=10)
+        plt.clf()
+        plt.quiver(x[::5, ::5], y[::5, ::5], u_st[::5, ::5], v_st[::5, ::5])
     else:
-        timelength = 86400
-        nt = 2000
+        timelength = 86400 * 30
+        #nt = 8640 * 2 * 1
+        nt = 8640 * 2 * 3
         eta0 = np.zeros_like(x)
         u0 = np.zeros((x.shape[0] + 1, x.shape[1]))
         v0 = np.zeros((x.shape[0], x.shape[1] + 1))
-        gyre_sim_jit = autojit(gyre_sim)
-        eta, u, v = gyre_sim(eta0, u0, v0, timelength, nt, x, y, **settings)
+        #settings['f0'] = 0
+        #settings['B'] = 0
+        #gyre_sim_jit = autojit(gyre_sim)
+        #settings['tau0'] = 20.
+
+        eta, u, v = gyre_sim(0, eta0, u0, v0, timelength, nt, x, y, **settings)
 
